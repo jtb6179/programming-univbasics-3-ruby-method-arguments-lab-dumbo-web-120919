94161934aa30ea8ca5f9fd8fc72d8a3e0586ba2f
super-model-4696 <jtb6179@g.rit.edu>
2019-11-20 20:54:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,11 +7,11 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language, optional)
-  puts "Hi, My name is #{name} and I love #{language}. however, I dislike #{optional}."
+  puts "Hi, My name is #{name} and I am learning to program in #{language}. however, I dislike #{optional}."
 end
 
 
